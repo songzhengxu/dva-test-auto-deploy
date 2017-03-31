553ec2f2dbc2d27260a3df9261cc528c828a3eed
songzhengxu <2944434952@qq.com>
2017-03-31 14:54:13 +0800

    修改服务器配置

--- src/services/users.js
+++ src/services/users.js
@@ -1,5 +1,5 @@
 import request from '../utils/request';
 import { PAGE_SIZE } from '../constants';
 export function fetch({ page = 1 }) {
-  return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
+  return request(`http://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${PAGE_SIZE}`);
 }
