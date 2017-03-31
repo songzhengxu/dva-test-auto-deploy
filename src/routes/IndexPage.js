95dbbb81ab3aeedacdac5e19ef87889c16c1596b
songzhengxu <2944434952@qq.com>
2017-03-31 14:37:25 +0800

    添加测试文件

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -6,7 +6,7 @@ function IndexPage({ location }) {
   return (
     <MainLayout location={location}>
       <div className={styles.normal}>
-        <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+        <h1 className={styles.title}>Yay! Welcome to dva</h1>
         <div className={styles.welcome} />
         <ul className={styles.list}>
           <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
---
+++ test/shallow.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { expect } from 'chai';
+import App from '../src/components/App';
+
+function shallowRender(Component) {
+  const renderer = TestUtils.createRenderer();
+  renderer.render(<Component/>);
+  return renderer.getRenderOutput();
+}
+
+describe('Shallow Rendering', function () {
+  it('App\'s title tag should is h1', function () {
+    const app = shallowRender(App);
+    expect(app.props.children.type).to.equal('div');
+  });
+});
